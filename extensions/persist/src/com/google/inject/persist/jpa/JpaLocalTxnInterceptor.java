2d162e3640b7dd064d40aebcd59d5b6dfc9ad3d0
Jarno Jantunen <jarno.jantunen@gmail.com>
2019-02-12 10:26:13 +0200

    fixed merge compile error

--- extensions/persist/src/com/google/inject/persist/jpa/JpaLocalTxnInterceptor.java
+++ extensions/persist/src/com/google/inject/persist/jpa/JpaLocalTxnInterceptor.java
@@ -1,26 +1,23 @@
 /*
  * Copyright (C) 2010 Google, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.google.inject.persist.jpa;
 
 import java.lang.reflect.Method;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
@@ -43,7 +40,6 @@ class JpaLocalTxnInterceptor implements MethodInterceptor {
   @Inject
   private UnitOfWorkService unitOfWork;
 
-  @Override
   @Override
   public Object invoke(MethodInvocation methodInvocation) throws Throwable {
 
@@ -122,8 +118,8 @@ class JpaLocalTxnInterceptor implements MethodInterceptor {
    * @param e The exception to test for rollback
    * @param txn A JPA Transaction to issue rollbacks on
    */
-  private boolean rollbackIfNecessary(
-      Transactional transactional, Exception e, EntityTransaction txn) {
+  private boolean rollbackIfNecessary(Transactional transactional, Exception e,
+      EntityTransaction txn) {
     boolean commit = true;
 
     // check rollback clauses
